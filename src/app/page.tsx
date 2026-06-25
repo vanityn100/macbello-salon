7c8dd284cbe6c080b8e65e91c37f6c8c241f53a5
Adonis John <vanitynstores@gmail.com>
2026-06-25 23:57:57 +0530

    style: temporarily hide Before & After section from landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,7 +38,7 @@ export default function Home() {
         <Services featuredOnly={true} />
 
         {/* Interactive Before & After comparison slider */}
-        <BeforeAfter />
+        {/* <BeforeAfter /> */}
 
         {/* Google Reviews Showcase Section (Preview mode) */}
         <Trust isHomepage={true} />
