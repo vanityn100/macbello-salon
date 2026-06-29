b35730122cce78b6f36ed053bfc0be3e16abd21e
Adonis John <vanitynstores@gmail.com>
2026-06-29 18:39:36 +0530

    Remove scratch folder to fix Vercel/Next.js typescript build errors

--- scratch/migration_staff_contribution.sql
+++
@@ -1,4 +0,0 @@
--- SQL Migration for Staff Contribution Column
--- Run this query in your Supabase SQL Editor:
-
-ALTER TABLE invoice_items ADD COLUMN IF NOT EXISTS staff_contribution VARCHAR(255);
--- scratch/update_trigger.js
+++
@@ -1,5 +0,0 @@
-require('dotenv').config({ path: '.env.local' });
-const { createClient } = require('@supabase/supabase-js');
-
-// We will try to call a standard RPC if we can't run raw SQL.
-console.log("Checking for SQL connection...");
